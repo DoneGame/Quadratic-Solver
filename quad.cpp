9648f0eb9999aca924fa45d3dfb3dd0af6859f12
DoneGame <d.geyzha@gmail.com>
2024-08-20 17:25:19 +0300

    2.0

--- quad.cpp
+++ quad.cpp
@@ -5,75 +5,109 @@
 const int INF_ROOTS = -1;
 const double precision = 0.00001;
 
-int SolveEquation (double a, double b, double c, double *x1, double *x2);
-int non_zero (double x);
+void GetCoefs(double *a, double *b, double *c);
+int SolveQuadraticEquation (double a, double b, double c, double *x1, double *x2);
+int SolveLinearEquation (double a, double b, double *x);
+void PrintResults (int n_roots, double x1, double x2);
+int NonZero (double x);
 
 int main() {
     double a = 0, b = 0, c = 0;
 
-    printf("# Second order equation solver\n");
-    printf("# a*x^2 + b*x + c = 0\n");
-    printf("# Enter a, b, c: ");
-    while (scanf("%lg %lg %lg", &a, &b, &c) != 3)
-        printf("Incorrect format. Enter a, b, c: ");
+    printf("# Second power equation solver\n");
+
+    GetCoefs (&a, &b, &c);
 
     double x1 = 0, x2 = 0;
-    int n_roots = SolveEquation (a, b, c, &x1, &x2);
+    int n_roots = SolveQuadraticEquation (a, b, c, &x1, &x2);
 
-    switch (n_roots) {
-        case 0: printf("No roots");
-        break;
+    PrintResults (n_roots, x1, x2);
 
-        case 1: printf("x = %.5lg", x1);
-        break;
+}
 
-        case 2: printf("x1 = %.5lg\n"
-                       "x2 = %.5lg", x1, x2);
-        break;
+void GetCoefs(double *a, double *b, double *c) {
 
-        case INF_ROOTS: printf("Infinite number of roots");
-        break;
+    printf("# a*x^2 + b*x + c = 0\n");
+    printf("# Enter a, b, c: ");
+    while (scanf("%lg %lg %lg", a, b, c) != 3) {
+        int s = '\0';
+        while ((s = getchar()) != '\n' && s != EOF);  // clear buffer
 
-        default: printf("main(): n_roots is incorrect");
+        printf("# Incorrect format. Enter a, b, c: ");
     }
 
 }
 
-int SolveEquation (double a, double b, double c,
-                   double *x1, double *x2) {
+int SolveQuadraticEquation (double a, double b, double c,
+                            double *x1, double *x2) {
 
     assert(isfinite(a));
     assert(isfinite(b));
     assert(isfinite(c));
     assert(x1 != x2);
 
-    if (non_zero(a)) {
+    if (NonZero(a)) {
         double D = b*b - 4*a*c;
-        double sqrt_D = sqrt(D);
 
-        *x1 = (-b + sqrt_D) / (2*a);
-        *x2 = (-b - sqrt_D) / (2*a);
+        if (NonZero(D)) {
+            if (D < 0) return 0;
 
-        assert(isfinite(*x1));
-        assert(isfinite(*x2));
+            double sqrt_D = sqrt(D);
 
-        if (D < 0) return 0;
+            *x1 = (-b + sqrt_D) / (2*a);
+            *x2 = (-b - sqrt_D) / (2*a);
 
-        if (non_zero(D)) return 2;
-        else return 1;
-    }
+            assert(isfinite(*x1));
+            assert(isfinite(*x2));
 
-    if (non_zero(b)) {
-        *x1 = *x2 = -c / b;
+            return 2;
+        }
+
+
+        *x1 = *x2 = -b / (2*a);
 
         assert(isfinite(*x1));
 
         return 1;
     }
 
-    if (non_zero(c)) return 0;
+    return SolveLinearEquation (b, c, x1);
+    *x2 = *x1;
+
+}
+
+int SolveLinearEquation (double a, double b, double *x) {
+
+    if (NonZero(a)) {
+        *x = -b / a;
+
+        assert(isfinite(*x));
+
+        return 1;
+    }
+
+    if (NonZero(b)) return 0;
     else return INF_ROOTS;
 
 }
 
-int non_zero (double x) {return (fabs(x) > precision) ? 1 : 0;}
+void PrintResults (int n_roots, double x1, double x2) {
+    switch (n_roots) {
+        case 0:         printf("No roots");
+                        break;
+
+        case 1:         printf("x = %.5lg", x1);
+                        break;
+
+        case 2:         printf("x1 = %.5lg\n"
+                               "x2 = %.5lg", x1, x2);
+                        break;
+
+        case INF_ROOTS: printf("Infinite number of roots");
+                        break;
+
+        default:        printf("PrintResults(): n_roots is incorrect");
+    }
+}
+
+int NonZero (double x) {return (fabs(x) > precision) ? 1 : 0;}
