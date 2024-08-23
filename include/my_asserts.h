ed3e04d921a16398f58ee7a3c6758a4511fc0b95
DoneGame <d.geyzha@gmail.com>
2024-08-23 17:29:29 +0300

    7.3.3 (minor changes in my_assert macros)

--- include/my_asserts.h
+++ include/my_asserts.h
@@ -17,30 +17,30 @@
 
 
 #if (!defined (NDEBUG) && !defined (DEACTIVATE_MY_ASSERTS))
-    #define my_assert(log_expr)                                                           \
-    if (!(log_expr)) {                                                                    \
-        ColorOn();                                                                        \
-        RedText();                                                                        \
-        printf("Assertion failed!\n");                                                    \
-        printf("File: %s, line: %d, function: %s()\n", __FILE__, __LINE__, __FUNCTION__); \
-        printf("Logical expression: %s\n", #log_expr);                                    \
-        DefaultText();                                                                    \
-        exit(1);                                                                          \
-    }                                                                                     \
-    else {}
+    #define my_assert(log_expr) {                                                             \
+        if (!(log_expr)) {                                                                    \
+            ColorOn();                                                                        \
+            RedText();                                                                        \
+            printf("Assertion failed!\n");                                                    \
+            printf("File: %s, line: %d, function: %s()\n", __FILE__, __LINE__, __FUNCTION__); \
+            printf("Logical expression: %s\n", #log_expr);                                    \
+            DefaultText();                                                                    \
+            exit(1);                                                                          \
+        }                                                                                     \
+    }
 
-    #define isfinite_assert(fp)                                                           \
-    if (!isfinite(fp)) {                                                                  \
-        ColorOn();                                                                        \
-        RedText();                                                                        \
-        printf("Assertion failed!\n");                                                    \
-        printf("Error: %s is not finite double!\n", #fp);                                 \
-        printf("File: %s, line: %d, function: %s()\n", __FILE__, __LINE__, __FUNCTION__); \
-        printf("Value of %s: %lg\n", #fp, (fp));                                          \
-        DefaultText();                                                                    \
-        exit(1);                                                                          \
-    }                                                                                     \
-    else {}
+    #define isfinite_assert(fp) {                                                             \
+        if (!isfinite(fp)) {                                                                  \
+            ColorOn();                                                                        \
+            RedText();                                                                        \
+            printf("Assertion failed!\n");                                                    \
+            printf("Error: %s is not finite double!\n", #fp);                                 \
+            printf("File: %s, line: %d, function: %s()\n", __FILE__, __LINE__, __FUNCTION__); \
+            printf("Value of %s: %lg\n", #fp, (fp));                                          \
+            DefaultText();                                                                    \
+            exit(1);                                                                          \
+        }                                                                                     \
+    }
 
 #else
     #define my_assert(log_expr)
